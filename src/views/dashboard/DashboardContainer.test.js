49eba96210d4b7a067b7919e111d070ba056350f
pessolatohenrique <pessolatohenrique@gmail.com>
2023-01-23 20:32:30 -0200

    general - refactoring unit test

--- src/views/dashboard/DashboardContainer.test.js
+++ src/views/dashboard/DashboardContainer.test.js
@@ -17,11 +17,11 @@ describe("Dashboard Container", () => {
     const password = screen.getByTestId("password");
 
     fireEvent.input(username, {
-      target: { value: process.env.REACT_APP_API_DEFAULT_USER_WITHOUT_RECIPE },
+      target: { value: process.env.REACT_APP_API_DEFAULT_USER },
     });
     fireEvent.input(password, {
       target: {
-        value: process.env.REACT_APP_API_DEFAULT_PASSWORD_WITHOUT_RECIPE,
+        value: process.env.REACT_APP_API_DEFAULT_EMAIL,
       },
     });
     fireEvent.click(submitBtn);
@@ -30,38 +30,5 @@ describe("Dashboard Container", () => {
   it("should generate snapshot", () => {
     const { container } = render(<DashboardContainer />);
     expect(container).toMatchSnapshot();
-  });
-
-  it("should show alert when don't have a recipe", async () => {
-    render(<DashboardContainer />);
-    const associateBtn = screen.getByTestId("associate-button");
-    expect(associateBtn).toBeInTheDocument();
-  });
-
-  it("should not show alert when have a recipe", async () => {
-    render(<App />);
-    const submitBtn = screen.getByTestId("submit-button");
-    const username = screen.getByTestId("username");
-    const password = screen.getByTestId("password");
-
-    fireEvent.input(username, {
-      target: { value: process.env.REACT_APP_API_DEFAULT_USER },
-    });
-    fireEvent.input(password, {
-      target: {
-        value: process.env.REACT_APP_API_DEFAULT_PASSWORD,
-      },
-    });
-    fireEvent.click(submitBtn);
-
-    await waitFor(() =>
-      expect(screen.queryByText("Associar")).not.toBeInTheDocument()
-    );
-  });
-
-  it("should load indicators cards", async () => {
-    render(<DashboardContainer />);
-    const indicatorCard = screen.getAllByTestId("indicator-card");
-    expect(indicatorCard.length).toBe(3);
   });
 });
--- src/views/register/LoginForm.test.js
+++ src/views/register/LoginForm.test.js
@@ -8,10 +8,7 @@ import {
 } from "@testing-library/react";
 import { renderHook } from "@testing-library/react-hooks";
 import { PROJECT_NAME } from "../../constants/default_settings";
-import {
-  USER_NOT_EXISTS_MESSAGE,
-  WRONG_PASSWORD_MESSAGE,
-} from "../../constants/messages";
+import { WRONG_PASSWORD_MESSAGE } from "../../constants/messages";
 import App from "../../App";
 import LoginForm from "./LoginForm";
 import useToken from "../../hooks/useToken";
@@ -38,25 +35,6 @@ describe("Login Form", () => {
     fireEvent.click(submitBtn);
 
     await waitForElement(() => screen.getByText(PROJECT_NAME));
-  });
-
-  it("should not login when user not exists", async () => {
-    const { setToken } = renderHook(() => useToken());
-    render(<LoginForm setToken={setToken} />);
-
-    const submitBtn = screen.getByTestId("submit-button");
-    const username = screen.getByTestId("username");
-    const password = screen.getByTestId("password");
-
-    fireEvent.input(username, {
-      target: { value: "usuarioquenaoexiste" },
-    });
-    fireEvent.input(password, {
-      target: { value: process.env.REACT_APP_API_DEFAULT_PASSWORD },
-    });
-    fireEvent.click(submitBtn);
-
-    await waitFor(() => expect(screen.getByText(USER_NOT_EXISTS_MESSAGE)));
   });
 
   it("should not login when password is invalid", async () => {
